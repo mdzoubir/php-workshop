c06fc4cdea6a9c500cdc1caec68be51c04476da4
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-05-31 23:18:02 +0100

    finalisation inoncé 3

--- inoncé3/activity-movies.php
+++ inoncé3/activity-movies.php
@@ -1,26 +1,28 @@
+
+
+
 <?php
 
 
-function show(){
-    $realis= [
-        'realisateur1' => ['film1', 'film2', 'film3', 'film4'],
-        'realisateur2' => ['film5', 'film6', 'film7', 'film8'],
-        'realisateur3' => ['film9', 'film10', 'film11', 'film12'],
-        'realisateur4' => ['film13', 'film14', 'film15', 'film16'],
-        'realisateur5' => ['film17', 'film18', 'film19', 'film20']
-    ];
-    $i = 0;
+function show($K, $V){
+    $realis= array(
+        'Spielberg' => ['Le pont des espions', 'The Post', 'film3', 'film4', 'filmX'],
+        'Christopher Nolan' => ['Dunkerque', 'Quai', 'film7', 'film8', 'filmY'],
+        'Martin Scorsese' => ['Le Loup de Wall Street', 'Hugo', 'film11', 'film12', 'filmZ'],
+        'realisateur4' => ['film13', 'film14', 'film15', 'film16', 'filmA'],
+        'realisateur5' => ['film17', 'film18', 'film19', 'film20', 'filmF']
+    );
+    $rand_key = array_rand($realis, $K);
+    $randV = array_rand($rand_key, $V);
     
-    $f = $i + 1;
-        foreach($realis as $key => $value){
-            echo "<h2>les films de $key </h2>";
-            echo "$value[$i] <br> $value[$f]"; 
-            $i++;
+        foreach($rand_key as $value){
+               echo "<h2>les films de $value :</h2>";
+            foreach($randV as $change){
+                echo $realis[$value][$change] . "<br>";
+            }
         }
         
-}show();
+}
+show(5,3);
 
 ?>
-
-
-
