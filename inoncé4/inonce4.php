d52e5543eb1b4b0a243d08b9731acab9726b7fa2
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-06-03 00:42:32 +0100

    Update inonce4.php

--- inoncé4/inonce4.php
+++ inoncé4/inonce4.php
@@ -1,11 +1,10 @@
 <?php
-//function  factorile
+//function  calculer la factorile d'un nomber
 $par1 = 5;
 function factoriel($par1){
       $fact = 1;
       for($i=$par1; $i>0;$i--){
-        echo "$fact * $i =";
-        echo $fact * $i ."<br>";
+        echo "$fact * $i =" .$fact * $i. "<br>";
         $fact = $fact * $i ;   
         
      }
@@ -13,32 +12,27 @@ function factoriel($par1){
    
 } factoriel($par1);
 
-
-// function some($v){
-//     $a = array();
-//     array_push($a, $v);
-//     echo "sum(a) = " . array_sum($a) . "\n";
-
-//     $b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
-//     echo "sum(b) = " . array_sum($b) . "\n";
-
-// }some(5);
-
-// function somme
+//function calculer la somme de plusuer valeur ;
 function somme(){
-    $a = array(4,5);
-    $end = end($a);
-      for($i=0; $i>=$end;$i++){
-        $result =  array_sum($a[$i]);
-        
-     }
-     echo  $result;
-     
-//    echo "<h4>la factorial de $par1! = $fact </h4>";
+  $a = array(4,5);
+  echo array_sum($a). "<br>" ;
 }somme();
 
 
-
+//fonction pour voir si un nombre entré en paramètre est premier ou non ;
+function premier($x){
+  for ($i=2; $i < $x ; $i++) { 
+    $mod = $x % $i;
+    if($mod==0){
+      $resulta =$x. " n'est pas un nombre premier.";
+        
+    }elseif($mod>=0){
+      $resulta =$x. "  est un nombre premier.";
+    }
+    echo "$x % $i = $mod <br>";
+  }
+  echo $resulta;
+}premier(10);
 
 
 
